9f7ddc8184f7c9f72d88539ee7b275f1d3523c8a
kts <akvks456@gmail.com>
2018-09-19 10:31:00 +0900

    set routes

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
+/*
+ *  Create by DaeHo Kim on 2018-09
+ */
+
+var http = require('http');
 var express = require('express');
+var path = require('path');
 var app = express();
-var winston = require('winston');
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({filename: 'combined.log'})
-  ]
+
+var routes = require('./routes');
+
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(info => {
+  return `${info.timestamp} [${info.level}]: ${info.message}`;
 });
 
-app.listen(3000);
-logger.info('Server Start');
+const logger = createLogger({
+  format: combine(
+    timestamp(),
+    myFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({filename: 'combined.log'})
+]
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/', routes);
+
+http.createServer(app).listen(3000);
+logger.info('Server Start')
+//
+// app.get('/', function(req, res){
+//   res.send("Welcome");
+// });
+//
+// app.listen(3000, function(){
+//   logger.info('Server Start');
+// });
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+// Main page
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+// Demo parameter setting page
+router.get('/demoReady', function(req, res) {
+  res.render('demoReady');
+});
+
+// Demo result page
+router.get('/demoResult', function(req, res) {
+  res.render('demoResult');
+});
+
+module.exports = router;
