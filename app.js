71220f4bec2669b08adc20d25a1d0389f6f9cf37
Daeho Kim <rlaeogh222@gmail.com>
2018-09-19 15:33:23 +0900

    set view directory

--- app.js
+++ app.js
@@ -31,23 +31,23 @@ const logger = createLogger({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + 'views/public'));
+app.use(express.static(__dirname + '/views/public'));
 app.use(bodyParser());
 app.use('/', routes);
 
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-		message: err.message,
-		error: err
-	});
-});
+// app.use(function(req, res, next) {
+// 	var err = new Error('Not Found');
+// 	err.status = 404;
+// 	next(err);
+// });
+//
+// app.use(function(err, req, res, next) {
+// 		res.status(err.status || 500);
+// 		res.render('error', {
+// 		message: err.message,
+// 		error: err
+// 	});
+// });
 
 http.createServer(app).listen(3000);
 logger.info('Server Start')
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(info => {
+  return `${info.timestamp} [${info.level}]: ${info.message}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    timestamp(),
+    myFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({filename: 'combined.log'})
+]
+});
+
+
 // Main page
 router.get('/', function(req, res) {
   res.render('index');
+  logger.info(req.ip + ' view Homepage');
 });
 
 // Demo parameter setting page
 router.get('/demoReady', function(req, res) {
   res.render('demoReady');
+  logger.info(req.ip + ' view demoReady');
 });
 
 // Demo result page
 router.post('/demoResult', function(req, res) {
   res.render('demoResult');
+  logger.info(req.ip + ' view demoResult');
 });
 
 module.exports = router;
