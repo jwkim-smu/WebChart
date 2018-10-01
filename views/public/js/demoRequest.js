8d8457b409c3ac3e2fdb6cd7ff12470751e1a566
Daeho Kim <rlaeogh222@gmail.com>
2018-10-01 13:25:22 +0900

    modulize demo request and result script

---
+++ views/public/js/demoRequest.js
@@ -0,0 +1,24 @@
+$("#param").submit(function(){
+  // read data
+  var date=$("input[name=date]").val();
+  var startTime=$("#startTime option:selected").val();
+  var endTime=$("#endTime option:selected").val();
+  // ajax
+  $.ajax({
+    type: "POST",
+    url: "/demoRequest",
+    data: {'date':date, 'startTime':startTime, 'endTime':endTime},
+    dataType:"json",
+    success: function(res){
+      if(res.err != null) {
+        console.log(res.err);
+      }
+      else {
+        // Chart
+        console.log(res.data);
+        demoResultChart(res.data);
+      }
+    }
+  });
+  return false; // prevent page move
+});
